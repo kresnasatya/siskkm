14e211ffb4968fc54203c2e75dbf1f6703ef3105
Putu Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2016-06-16 12:23:20 +0800

    add jenis_model, tingkat_model, sebagai_model

--- application/controllers/admin/Bobotskkm.php
+++ application/controllers/admin/Bobotskkm.php
@@ -6,14 +6,19 @@ class Bobotskkm extends Admin_Controller{
   public function __construct()
   {
     parent::__construct();
-
+    $this->load->model('admin/Jenis_model','jenis');
+    $this->load->model('admin/Tingkat_model','tingkat');
+    $this->load->model('admin/Sebagai_model','sebagai');
     $this->load->library('form_validation');
   }
 
   function index()
   {
-
-    $data = array('current_user' => $this->ion_auth->user()->row());
+    $data = array(
+                  'current_user' => $this->ion_auth->user()->row(),
+                  'jenis' => $this->jenis->get_all(),
+                  'tingkat' => $this->tingkat->get_all(),
+                  'sebagai' => $this->sebagai->get_all());
     $this->template->load('templates/admin/bobot_template','admin/bobotskkm/list',$data);
   }
 
--- application/views/admin/bobotskkm/list.php
+++ application/views/admin/bobotskkm/list.php
@@ -25,16 +25,45 @@
             </button>
           </div>
           <!-- /.box-tools -->
+
         </div>
         <div class="box-body">
-          <div class="col-md-4">
-            <a href="" class="btn btn-primary">Tambah Jenis</a>
+          <div class="row">
+            <div class="col-md-4">
+              <a href="" class="btn btn-primary">Tambah Jenis</a>
+            </div>
+            <div class="col-md-4 text-center">
+                <div style="margin-top: 4px"  id="message">
+                    <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
+                </div>
+            </div>
           </div>
-          <div class="col-md-4 text-center">
-              <div style="margin-top: 4px"  id="message">
-                  <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
-              </div>
-          </div>
+          <br>
+          <table id="jenistable" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Jenis Kegiatan</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+                    $start = 0;
+                    foreach ($jenis as $row): ?>
+                    <tr>
+                      <td><?php echo ++$start; ?></td>
+                      <td><?php echo $row->jenis; ?></td>
+                      <td><?php
+                              $hapus = array(
+                                        'class' => 'btn btn-sm btn-danger',
+                                        'onclick' => 'javascript: return confirm(\'Kamu Yakin ?\')'
+                                      );
+                                      echo anchor(site_url('admin/bobotskkm/hapus_jenis/'.$row->id),'Hapus',$hapus);?></td>
+                    </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
         </div>
       </div>
       <!-- End Jenis -->
@@ -51,14 +80,44 @@
           <!-- /.box-tools -->
         </div>
         <div class="box-body">
-          <div class="col-md-4">
-            <a href="" class="btn btn-primary">Tambah Tingkat</a>
+          <div class="row">
+            <div class="col-md-4">
+              <a href="" class="btn btn-primary">Tambah Tingkat</a>
+            </div>
+            <div class="col-md-4 text-center">
+                <div style="margin-top: 4px"  id="message">
+                    <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
+                </div>
+            </div>
           </div>
-          <div class="col-md-4 text-center">
-              <div style="margin-top: 4px"  id="message">
-                  <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
-              </div>
-          </div>
+          <br>
+          <table id="tingkattable" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Jenis</th>
+                <th>Tingkat</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+                    $start = 0;
+                    foreach ($tingkat as $row): ?>
+                    <tr>
+                      <td><?php echo ++$start; ?></td>
+                      <td><?php echo $row->jenis; ?></td>
+                      <td><?php echo $row->tingkat; ?></td>
+                      <td><?php
+                              $hapus = array(
+                                        'class' => 'btn btn-sm btn-danger',
+                                        'onclick' => 'javascript: return confirm(\'Kamu Yakin ?\')'
+                                      );
+                                      echo anchor(site_url('admin/bobotskkm/hapus_tingkat/'.$row->id_tingkat),'Hapus',$hapus);?></td>
+                    </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
         </div>
       </div>
       <!-- End Tingkat -->
@@ -75,14 +134,46 @@
           <!-- /.box-tools -->
         </div>
         <div class="box-body">
-          <div class="col-md-4">
-            <a href="" class="btn btn-primary">Tambah Sebagai</a>
+          <div class="row">
+            <div class="col-md-4">
+              <a href="" class="btn btn-primary">Tambah Sebagai</a>
+            </div>
+            <div class="col-md-4 text-center">
+                <div style="margin-top: 4px"  id="message">
+                    <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
+                </div>
+            </div>
           </div>
-          <div class="col-md-4 text-center">
-              <div style="margin-top: 4px"  id="message">
-                  <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
-              </div>
-          </div>
+          <br>
+          <table id="sebagaitable" class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Tingkat</th>
+                <th>Sebagai</th>
+                <th>Bobot</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+                    $start = 0;
+                    foreach ($sebagai as $row): ?>
+                    <tr>
+                      <td><?php echo ++$start; ?></td>
+                      <td><?php echo $row->tingkat; ?></td>
+                      <td><?php echo $row->sebagai; ?></td>
+                      <td><?php echo $row->bobot; ?></td>
+                      <td><?php
+                              $hapus = array(
+                                        'class' => 'btn btn-sm btn-danger',
+                                        'onclick' => 'javascript: return confirm(\'Kamu Yakin ?\')'
+                                      );
+                                      echo anchor(site_url('admin/bobotskkm/hapus_sebagai/'.$row->id_sebagai),'Hapus',$hapus);?></td>
+                    </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
         </div>
       </div>
       <!-- End Sebagai -->
