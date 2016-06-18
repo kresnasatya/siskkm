f0269bf1065555d69e74a4ac721b7c1b8a0f7752
Putu Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2016-06-18 13:02:51 +0800

    update kontak view

--- application/views/kontak.php
+++ application/views/kontak.php
@@ -15,16 +15,16 @@
 	 <div class="container mtb">
 	 	<div class="row">
 	 		<div class="col-lg-8">
-	 			<h4>Kritik dan Saran!</h4>
+	 			<h4>Kritik dan Saran</h4>
 	 			<div class="hline"></div>
-		 			<p>Pengembang sangat terbuka dengan kritik dan saran yang mebangun untuk sistem informasi ini.</p>
+		 			<p>Pengembang sangat terbuka dengan kritik dan saran yang membangun untuk sistem informasi ini.</p>
 		 			<form role="form">
 					  <div class="form-group">
-					    <label for="InputName1">Nama Kamu</label>
+					    <label for="InputName1">Nama</label>
 					    <input type="text" name="nama" class="form-control" id="exampleInputEmail1" required="">
 					  </div>
 					  <div class="form-group">
-					    <label for="InputEmail1">Email Kamu</label>
+					    <label for="InputEmail1">Email</label>
 					    <input type="email" name="email" class="form-control" id="exampleInputEmail1" required="">
 					  </div>
 					  <div class="form-group">
@@ -35,12 +35,12 @@
 					  	<label for="message1">Pesan</label>
 					  	<textarea class="form-control" name="pesan" id="message1" rows="3" required=""></textarea>
 					  </div>
-					  <button type="submit" class="btn btn-theme">Submit</button>
+					  <button type="submit" class="btn btn-theme">Kirim</button>
 					</form>
 			</div><! --/col-lg-8 -->
 
 	 		<div class="col-lg-4">
-		 		<h4>Alamat Kami</h4>
+		 		<h4>Alamat</h4>
 		 		<div class="hline"></div>
 		 			<p>
 		 				Politeknik Negeri Bali,<br/>
