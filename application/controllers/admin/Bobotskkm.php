a49cd50a3e2198ab4d66d0289e30ad06fe8b346c
Putu Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2016-06-19 19:41:05 +0800

    delete file jenis_model, sebagai_model, tingkat model and combine them to bobotskkm_model file and also update bobotskkm controller

--- application/controllers/admin/Bobotskkm.php
+++ application/controllers/admin/Bobotskkm.php
@@ -7,9 +7,7 @@ class Bobotskkm extends Admin_Controller
   public function __construct()
   {
     parent::__construct();
-    $this->load->model('admin/Jenis_model','jenis');
-    $this->load->model('admin/Tingkat_model','tingkat');
-    $this->load->model('admin/Sebagai_model','sebagai');
+    $this->load->model('admin/Bobotskkm_model','bobot');
     $this->load->library('form_validation');
   }
 
@@ -17,9 +15,9 @@ class Bobotskkm extends Admin_Controller
   {
     $data = array(
                   'current_user' => $this->ion_auth->user()->row(),
-                  'jenis' => $this->jenis->get_all(),
-                  'tingkat' => $this->tingkat->get_all(),
-                  'sebagai' => $this->sebagai->get_all()
+                  'jenis' => $this->bobot->get_all_jenis(),
+                  'tingkat' => $this->bobot->get_all_tingkat(),
+                  'sebagai' => $this->bobot->get_all_sebagai()
     );
     $this->template->load('templates/admin/bobot_template','admin/bobotskkm/list',$data);
   }
---
+++ application/models/admin/Bobotskkm_model.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Bobotskkm_model extends CI_Model {
+
+  public function __construct()
+  {
+    parent::__construct();
+
+  }
+
+  /*---------- Jenis -----------*/
+  public function get_all_jenis()
+  {
+    $sql = 'SELECT * FROM jenis';
+    return $this->db->query($sql)->result();
+  }
+  /*---------- End Jenis -----------*/
+
+  /*---------- Tingkat -----------*/
+  public function get_all_tingkat()
+  {
+    $sql = "SELECT id_tingkat, jenis.jenis, tingkat FROM tingkat
+            INNER JOIN jenis
+            ON tingkat.id_jenis_fk = jenis.id_jenis";
+    return $this->db->query($sql)->result();
+  }
+  /*---------- End Tingkat -----------*/
+
+  /*---------- Sebagai -----------*/
+  public function get_all_sebagai()
+  {
+    $sql = "SELECT id_sebagai, tingkat.tingkat, sebagai, bobot FROM sebagai
+            INNER JOIN tingkat
+            ON tingkat.id_tingkat = sebagai.id_tingkat_fk";
+    return $this->db->query($sql)->result();
+  }
+  /*---------- End Sebagai -----------*/
+
+}
