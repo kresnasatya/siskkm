07af8df51189315c19204eb0416a57dd78263619
Putu Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2016-06-29 16:34:03 +0800

    feat: Add feature kirim pesan in kontak page

--- application/views/kontak.php
+++ application/views/kontak.php
@@ -12,27 +12,30 @@
 	 	<div class="row">
 	 		<div class="col-lg-8">
 	 			<h4>Kritik dan Saran</h4>
+				<?php echo $this->session->userdata('email_sent') <> '' ? $this->session->userdata('email_sent') : ''; ?>
 	 			<div class="hline"></div>
 		 			<p>Pengembang sangat terbuka dengan kritik dan saran yang membangun untuk sistem informasi ini.</p>
-		 			<form role="form">
-					  <div class="form-group">
-					    <label for="InputName1">Nama</label>
-					    <input type="text" name="nama" class="form-control" id="exampleInputEmail1" required="">
-					  </div>
-					  <div class="form-group">
-					    <label for="InputEmail1">Email</label>
-					    <input type="email" name="email" class="form-control" id="exampleInputEmail1" required="">
-					  </div>
-					  <div class="form-group">
-					    <label for="InputSubject1">Perihal</label>
-					    <input type="text" name="perihal" value="Kritik dan Saran" class="form-control" id="exampleInputEmail1" readonly="">
-					  </div>
-					  <div class="form-group">
-					  	<label for="message1">Pesan</label>
-					  	<textarea class="form-control" name="pesan" id="message1" rows="3" required=""></textarea>
-					  </div>
-					  <button type="submit" class="btn btn-theme">Kirim</button>
-					</form>
+					<?php
+					$attributes = array('role' => 'form');
+					echo form_open('kontak/kirim_pesan', $attributes); ?>
+						<div class="form-group">
+							<label for="nama">Nama</label>
+							<input type="text" name="nama" class="form-control" id="nama" required="">
+						</div>
+						<div class="form-group">
+							<label for="email">Email</label>
+							<input type="email" name="email" class="form-control" id="email" required="">
+						</div>
+						<div class="form-group">
+							<label for="perihal">Perihal</label>
+							<input type="text" name="perihal" value="Kritik dan Saran" class="form-control" id="perihal" readonly="">
+						</div>
+						<div class="form-group">
+							<label for="pesan">Pesan</label>
+							<textarea class="form-control" name="pesan" id="pesan" rows="3" required=""></textarea>
+						</div>
+						<?php echo form_submit('submit', 'Kirim', 'class="btn btn-theme"'); ?>
+					<?php echo form_close(); ?>
 			</div><! --/col-lg-8 -->
 
 	 		<div class="col-lg-4">
