4a49a9568ddc5d16f9302fefc391e5f8d0f0b8cf
Putu Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2016-07-03 20:16:14 +0800

    add: Add gravatar function in admin controller and change message flashdata

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -20,10 +20,26 @@ class Admin_Controller extends MY_Controller
   {
     parent::__construct();
     if (!$this->ion_auth->logged_in()) {
-      $this->session->set_flashdata('message','Kamu harus jadi admin untuk login!');
+      $this->session->set_flashdata('message', 'Kamu bukan admin!');
       redirect('login');
     }
+      $event = 'logged_in';
+      $class = 'Admin_Controller';
+
+      $name = 'get_gravatar_hash';
+      $method = '_gravatar';
+      $this->ion_auth->set_hook($event, $name, $class, $method, array());
   }
+
+  public function _gravatar()
+    {
+        if($this->form_validation->valid_email($_SESSION['email']))
+        {
+            $gravatar_url = md5(strtolower(trim($_SESSION['email'])));
+            $_SESSION['gravatar'] = $gravatar_url;
+        }
+        return TRUE;
+    }
 
 }
 
@@ -37,7 +53,7 @@ class UP2KK_Controller extends MY_Controller
   {
     parent::__construct();
     if (!$this->ion_auth->in_group('up2kk')) {
-      $this->session->set_flashdata('message','Kamu harus jadi up2kk untuk login!');
+      $this->session->set_flashdata('message','Kamu bukan up2kk!');
       redirect('login');
     }
   }
@@ -54,7 +70,7 @@ class Mahasiswa_Controller extends MY_Controller
   {
     parent::__construct();
     if (!$this->ion_auth->in_group('mahasiswa')) {
-      $this->session->set_flashdata('message','Kamu harus jadi mahasiswa untuk login!');
+      $this->session->set_flashdata('message','Kamu bukan mahasiswa!');
       redirect('login');
     }
   }
