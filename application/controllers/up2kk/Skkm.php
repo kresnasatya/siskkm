37c0ca9fda9f64c6807daa6aef5a9b7a23e0953b
Putu Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2016-07-04 11:15:28 +0800

    refactor: Rename extends class.

--- application/controllers/up2kk/Skkm.php
+++ application/controllers/up2kk/Skkm.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Skkm extends CI_Controller{
+class Skkm extends UP2KK_Controller{
 
   public function __construct()
   {
